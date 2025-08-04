5393d7cdaa14ecca5d2263e3dde4542e6adae60f
Esteve Yann <esteveabel800@gmail.com>
2025-08-04 22:01:26 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -24,11 +24,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  experimental: {
-    // This is to allow the Next.js dev server to be proxied in the preview environment.
-    // @ts-ignore
-    allowedDevOrigins: ["**/**"],
-  }
 };
 
 export default nextConfig;
